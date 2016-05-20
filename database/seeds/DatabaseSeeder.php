9574b8ae99714937e9a0f7cf3a3a6f2cbe79b725
Alvin Pacot <aepacot2016@gmail.com>
2016-05-20 12:50:37 +0800

    updated seed now with users type and phone

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,8 +19,8 @@ class DatabaseSeeder extends Seeder
         DB::table('users')->delete();
 
         $users = array(
-            ['name' => 'Alvin Pacot', 'email' => 'admin@gmail.com', 'password' => Hash::make('secret')],
-            ['name' => 'Name Lastname', 'email' => 'user1@gmail.com', 'password' => Hash::make('secret')]
+            ['name' => 'Alvin Pacot', 'email' => 'admin@gmail.com', 'password' => Hash::make('secret'), 'users_type' => 'admin', 'phone' => '+639754882784'],
+            ['name' => 'Name Lastname', 'email' => 'user1@gmail.com', 'password' => Hash::make('secret'), 'users_type' => 'member']
     	);
             
         // Loop through each user above and create the record for them in the database
